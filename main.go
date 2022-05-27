b6dbf3f9fc355d3e335c19da4746b376ef3aa2cf
Sergey Fedotov <sflechie@gmail.com>
2022-05-27 18:47:12 +0800

    Implement RSI strategy trading

--- main.go
+++ main.go
@@ -5,26 +5,40 @@ import (
 	"fmt"
 	"github.com/0fs/cryprotrader/utils"
 	"github.com/adshao/go-binance/v2"
+	"github.com/cinar/indicator"
 	"github.com/rs/zerolog/log"
+	"strconv"
+	"time"
 )
 
-var klineCloses []float64
-var klineHigh []float64
-var klineLow []float64
+var asset indicator.Asset
 
 func main() {
-	initComponents()
-	getRecentKlines()
-	getIndicators()
 
+	initComponents()
+
+	firstFinalKline := true
 	wsKlineHandler := func(event *binance.WsKlineEvent) {
 
 		if event.Kline.IsFinal {
-			//log.Info().Msgf("Final kline: %s %s", event.Kline.Open, event.Kline.Close)
-			klineCloses = append(klineCloses[1:], utils.Stf(event.Kline.Close))
-			klineHigh = append(klineHigh[1:], utils.Stf(event.Kline.High))
-			klineLow = append(klineLow[1:], utils.Stf(event.Kline.Low))
-			getIndicators()
+
+			// Time syncronization
+			if firstFinalKline {
+				initRecentKlines()
+				firstFinalKline = false
+			}
+
+			t, _ := time.Parse("", strconv.FormatInt(event.Kline.StartTime, 10))
+			asset.Date = append(asset.Date[1:], t)
+			asset.Low = append(asset.Low[1:], utils.Stf(event.Kline.Low))
+			asset.High = append(asset.High[1:], utils.Stf(event.Kline.High))
+			asset.Opening = append(asset.Opening[1:], utils.Stf(event.Kline.Open))
+			asset.Closing = append(asset.Closing[1:], utils.Stf(event.Kline.Close))
+
+			actions := indicator.RsiStrategy(asset, 70, 30)
+
+			// Trade by the latest action
+			trade(actions[len(actions)-1])
 		}
 	}
 
@@ -46,20 +60,24 @@ func initComponents() {
 	initSpotConnection()
 }
 
-func trade(side binance.SideType) {
-	q := "0.00035" // ~10$
-	err := spotClient.NewCreateOrderService().Symbol(symbol).
-		Side(side).Type(binance.OrderTypeMarket).Quantity(q).Test(context.Background())
+func trade(action indicator.Action) {
 
-	if err != nil {
-		logger.Fatal().Err(err).Msg("")
+	if action == indicator.HOLD {
+		logger.Info().Msgf("Hold")
+		return
+	}
+
+	side := binance.SideTypeBuy
+	if action == indicator.SELL {
+		side = binance.SideTypeSell
 	}
 
 	order, err := spotClient.NewCreateOrderService().Symbol(symbol).
-		Side(side).Type(binance.OrderTypeMarket).Quantity(q).Do(context.Background())
+		Side(side).Type(binance.OrderTypeMarket).Quantity(qty).Do(context.Background())
 
 	if err != nil {
-		logger.Fatal().Err(err).Msg("")
+		logger.Error().Err(err).Msg("Could not create the the order")
+		return
 	}
 
 	logger.Info().Msgf("OrderID: %d | Price: %s", order.OrderID, order.Price)
--- utils/utils.go
+++ utils/utils.go
@@ -3,6 +3,7 @@ package utils
 import (
 	"fmt"
 	"github.com/rs/zerolog/log"
+	"math"
 	"strconv"
 )
 
@@ -17,4 +18,47 @@ func Stf(s string) float64 {
 
 func Fts(f float64) string {
 	return fmt.Sprintf("%.8f", f)
+}
+
+func StochRsi(period int, rsi []float64) ([]float64, []float64, []float64) {
+	stochRsi := make([]float64, len(rsi))
+	smoothK := make([]float64, len(rsi))
+	smoothD := make([]float64, len(rsi))
+
+	var min, max float64
+	for i := 0; i < len(rsi); i++ {
+		max = -1.0
+		min = 101.0
+		d := IntMax(i-period, 0)
+		for j := i; j > d; j-- {
+			min = math.Min(min, rsi[j])
+			max = math.Max(max, rsi[j])
+		}
+
+		stochRsi[i] = (rsi[i] - min) / (max - min) * 100
+
+		// SmoothK
+		d = IntMax(i-3, 0)
+		s := 0.0
+		for j := i; j > d; j-- {
+			s += stochRsi[j]
+		}
+		smoothK[i] = s / 3.0
+
+		// SmoothD
+		s = 0.0
+		for j := i; j > d; j-- {
+			s += smoothK[j]
+		}
+		smoothD[i] = s / 3.0
+	}
+
+	return stochRsi, smoothK, smoothD
+}
+
+func IntMax(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
 }
